87ae981bc9bff6bb41625bb8666f3c4057347ca7
Ahmad Nabeel Butt <ahmadnabeel11@hotmail.com>
2025-03-22 15:35:09 -0700

    fix

--- routes/OrganizationRoutes.js
+++ routes/OrganizationRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addOrganization, updateOrganization, deleteOrganization, getOrganizations } = require("../controllers/organizationController");
+const { addOrganization, updateOrganization, deleteOrganization, getOrganizations } = require("../controllers/OrganizationController");
 const authMiddleware = require("../middlewares/authMiddleware");
 
 const router = express.Router();
