57e7cbbc8373d623849f3909e94b3e543ecb9a5e
Ahmad Nabeel Butt <ahmadnabeel11@hotmail.com>
2025-04-05 12:18:48 +0500

    updated contact model

--- models/Contact.js
+++ models/Contact.js
@@ -3,6 +3,11 @@ const sequelize = require("../config/database");
 
 const Contact = sequelize.define("Contact", {
 
+  contact_id: { 
+    type: DataTypes.UUID, // UUID as the primary unique identifier for the invoice
+    defaultValue: DataTypes.UUIDV4,  // Automatically generate a UUID if not provided
+    primaryKey: true,  // Set as the primary key of the invoice
+  },
   organization_id: { 
     type: DataTypes.INTEGER, 
     allowNull: true 
